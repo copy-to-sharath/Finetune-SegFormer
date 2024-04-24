1b9ced1763a311db72e095edd3d749eaad221dde
sleepreap <98008874+sleepreap@users.noreply.github.com>
2024-04-24 11:09:11 +0800

    Update train.py

--- scripts/train.py
+++ scripts/train.py
@@ -2,23 +2,32 @@ import pytorch_lightning as pl
 import torch
 torch.manual_seed(1)
 torch.set_float32_matmul_precision("medium")
-from segformer.model import SegformerFinetuner
-from segformer.dataset import SegmentationDataModule
-import segformer.config as config
+from segformer import ( SegformerFinetuner, 
+                        SegmentationDataModule, 
+                        DATASET_DIR, 
+                        BATCH_SIZE, 
+                        NUM_WORKERS, 
+                        ID2LABEL, 
+                        LEARNING_RATE, 
+                        LOGGER, 
+                        PRECISION, 
+                        DEVICES, 
+                        EARLY_STOPPING_CALLBACK, 
+                        CHECKPOINT_CALLBACK, 
+                        EPOCHS )
 
 
 if __name__=="__main__":
-    data_module = SegmentationDataModule(dataset_dir=config.DATASET_DIR, batch_size=config.BATCH_SIZE, num_workers=config.NUM_WORKERS)
-    model=SegformerFinetuner(config.ID2LABEL, config.LEARNING_RATE)
+    data_module = SegmentationDataModule(dataset_dir=DATASET_DIR, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS)
+    model=SegformerFinetuner(ID2LABEL, LEARNING_RATE)
 
     trainer = pl.Trainer(
-        logger=config.LOGGER,
-        precision=config.PRECISION,
+        logger=LOGGER,
+        precision=PRECISION,
         accelerator='cuda',
-        devices=config.DEVICES,
-        strategy="ddp",
-        callbacks=[config.EARLY_STOPPING_CALLBACK, config.CHECKPOINT_CALLBACK],
-        max_epochs=config.EPOCHS
+        devices=DEVICES,
+        callbacks=[EARLY_STOPPING_CALLBACK, CHECKPOINT_CALLBACK],
+        max_epochs=EPOCHS
     )
     print("Training starts!!")
     trainer.fit(model,data_module)
