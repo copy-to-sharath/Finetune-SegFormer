e1617ea715991a8a566255b7ad068fe9eea5ec3a
sleepreap <98008874+sleepreap@users.noreply.github.com>
2024-05-21 21:49:24 +0800

    Update inference.py

--- scripts/utilities/inference.py
+++ scripts/utilities/inference.py
@@ -20,36 +20,38 @@ from tqdm import tqdm
 from colorPalette import color_palette, apply_palette
 
 def dataset_predictions(dataloader):
-    pred_set=[]
-    for batch in tqdm((dataloader), desc="Doing predictions"):
+    pred_set = []
+    for batch in tqdm(dataloader, desc="Doing predictions"):
         images, labels = batch['pixel_values'], batch['labels']
         outputs = model(images, labels)
         loss, logits = outputs[0], outputs[1]
         upsampled_logits = nn.functional.interpolate(
             logits,
-            #size of original image is 640x640
+            # Size of original image is 640x640
             size=labels.shape[-2:],
             mode="bilinear",
             align_corners=False
         )
         predicted_mask = upsampled_logits.argmax(dim=1).numpy()
-        pred_set.extend(predicted_mask) 
+        # Extend the pred_set list with each image in the batch
+        for image in predicted_mask:
+            pred_set.append(image)
     return pred_set
+
 
 def savePredictions(pred_set, save_path):
     palette = color_palette()
     for i, image in enumerate(tqdm(pred_set, desc="Saving predictions")):
         file_name = f"result_{i}"
         colored_image = apply_palette(image, palette)
-        plt.imshow(colored_image)  
+        plt.imshow(colored_image)
         plt.axis('off')  # Turn off axis numbers and ticks
-        # Construct the full path where the image will be saved
         file_path = os.path.join(save_path, f"{file_name}.png")
-        # Save the figure
         plt.savefig(file_path, bbox_inches='tight')
         plt.close()  # Close the figure to free memory
         
     print("Predictions saved")
+
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
@@ -75,8 +77,8 @@ if __name__=="__main__":
         
     data_module = SegmentationDataModule(dataset_dir=DATASET_DIR, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS)
     model = SegformerFinetuner.load_from_checkpoint(model_path,id2label=ID2LABEL, lr=LEARNING_RATE)
-    
     model.eval()
+
     data_module.setup(stage='test')
     test_dataloader = data_module.test_dataloader()
     pred_set= dataset_predictions(test_dataloader)
